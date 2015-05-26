65ed5a9a5999f7c61884f865bd63cc76dcddfa25
taturou <taturou@gmail.com>
2015-05-26 20:19:47 +0900

    Drawing a time with old-generation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <pebble.h>
 #include "xevi_number_layer.h"
 
+#define COLOR_BG GColorBlack
+
 #define NUM_XEVI_LAYER (6)
 #define TM_HOUR_0      (0)
 #define TM_HOUR_1      (1)
@@ -13,10 +15,11 @@ static Window *s_window;
 static TextLayer *s_text_layer;
 static XeviNumberLayer *s_xevi_layer[NUM_XEVI_LAYER];
 static char s_str[32];
-static bool draw_now_time = false;
 
 static void s_select_click_handler(ClickRecognizerRef recognizer, void *context) {
-    draw_now_time = draw_now_time == true ? false : true;
+    bool hidden = layer_get_hidden(text_layer_get_layer(s_text_layer));
+    hidden = hidden == true ? false : true;
+    layer_set_hidden(text_layer_get_layer(s_text_layer), hidden);
 }
 
 static void s_up_click_handler(ClickRecognizerRef recognizer, void *context) {
@@ -38,12 +41,8 @@ static void s_click_config_provider(void *context) {
 }
 
 static void s_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-    if (draw_now_time == true) {
-        snprintf(s_str, 32, "%d:%02d:%02d", tick_time->tm_hour, tick_time->tm_min, tick_time->tm_sec);
-        text_layer_set_text(s_text_layer, s_str);
-    } else {
-        text_layer_set_text(s_text_layer, " ");
-    }
+    snprintf(s_str, 32, "%d:%02d:%02d", tick_time->tm_hour, tick_time->tm_min, tick_time->tm_sec);
+    text_layer_set_text(s_text_layer, s_str);
 
     xevi_number_layer_set_number(s_xevi_layer[TM_HOUR_0], tick_time->tm_hour / 10);
     xevi_number_layer_set_number(s_xevi_layer[TM_HOUR_1], tick_time->tm_hour % 10);
@@ -56,16 +55,6 @@ static void s_tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 static void s_window_load(Window *window) {
     Layer *window_layer = window_get_root_layer(window);
     GRect bounds = layer_get_bounds(window_layer);
-
-    // text-layer
-    s_text_layer = text_layer_create(GRect(0, 0, bounds.size.w, 20));
-#if PBL_PLATFORM_BASALT
-    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS));
-#else
-    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
-#endif
-    text_layer_set_text_alignment(s_text_layer, GTextAlignmentLeft);
-    layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 
     // xevious-number-layer
     GRect frames[NUM_XEVI_LAYER] = {
@@ -80,6 +69,19 @@ static void s_window_load(Window *window) {
         s_xevi_layer[i] = xevi_number_layer_create(frames[i]);
         layer_add_child(window_layer, xevi_number_layer_get_layer(s_xevi_layer[i]));
     }
+
+    // text-layer
+    s_text_layer = text_layer_create(GRect(0, 0, bounds.size.w, 20));
+#if PBL_PLATFORM_BASALT
+    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_LECO_20_BOLD_NUMBERS));
+#else
+    text_layer_set_font(s_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+#endif
+    text_layer_set_text_alignment(s_text_layer, GTextAlignmentLeft);
+    text_layer_set_background_color(s_text_layer, GColorClear);
+    text_layer_set_text_color(s_text_layer, GColorWhite);
+    layer_set_hidden(text_layer_get_layer(s_text_layer), true);
+    layer_add_child(window_layer, text_layer_get_layer(s_text_layer));
 
     // tick-timer-service
     tick_timer_service_subscribe(SECOND_UNIT, s_tick_handler);
@@ -101,6 +103,10 @@ static void s_window_unload(Window *window) {
 static void s_init(void) {
     s_window = window_create();
     window_set_click_config_provider(s_window, s_click_config_provider);
+    window_set_background_color(s_window, COLOR_BG);
+#if PBL_PLATFORM_APLITE
+    window_set_fullscreen(s_window, true);
+#endif
     window_set_window_handlers(s_window, (WindowHandlers) {
         .load = s_window_load,
         .unload = s_window_unload,
--- src/xevi_number_layer.c
+++ src/xevi_number_layer.c
@@ -3,22 +3,25 @@
 
 #define STROKE_WIDTH (5)
 
+#define GEN          (2)
+#define GEN_NEW      (0)
+#define GEN_OLD      (1)
+
+#if PBL_COLOR
+ #define COLOR_GEN_NEW GColorRed
+ #define COLOR_GEN_OLD GColorDarkGray
+#else
+ #define COLOR_GEN_NEW GColorWhite
+ #define COLOR_GEN_OLD GColorBlack
+#endif
+    
 typedef struct XeviNumberLayer {
     Layer *layer;
-    uint8_t number;
-    GColor color;
+    uint8_t number[GEN];
+    GColor color[GEN];
 } XeviNumberLayer;
 
-static void s_layer_update_proc(struct Layer *layer, GContext *ctx) {
-    XeviNumberLayer *xevi_layer = (XeviNumberLayer*)layer_get_data(layer);
-    GRect bounds = layer_get_bounds(layer);
-
-#if PBL_PLATFORM_BASALT
-    graphics_context_set_antialiased(ctx, true);
-    graphics_context_set_stroke_width(ctx, STROKE_WIDTH);
-#endif
-    graphics_context_set_stroke_color(ctx, xevi_layer->color);
-
+static void s_draw_number(GContext *ctx, GRect bounds, uint8_t number) {
     // points
     GPoint tl = GPoint(0, 0);
     GPoint tr = GPoint(bounds.size.w - 1, 0);
@@ -26,23 +29,23 @@ static void s_layer_update_proc(struct Layer *layer, GContext *ctx) {
     GPoint br = GPoint(bounds.size.w - 1, bounds.size.h - 1);
 
     // draw
-    if (xevi_layer->number <= XEVI_NUMBER_MAX) {
+    if (number <= XEVI_NUMBER_MAX) {
         // 0
         graphics_draw_line(ctx, tr, bl);
         // 1
-        if ((xevi_layer->number & 0x1) == 0x1) {
+        if ((number & 0x1) == 0x1) {
             graphics_draw_line(ctx, bl, br);
         }
         // 2
-        if ((xevi_layer->number & 0x2) == 0x2) {
+        if ((number & 0x2) == 0x2) {
             graphics_draw_line(ctx, br, tr);
         }
         // 4
-        if ((xevi_layer->number & 0x4) == 0x4) {
+        if ((number & 0x4) == 0x4) {
             graphics_draw_line(ctx, tr, tl);
         }
         // 8
-        if ((xevi_layer->number & 0x8) == 0x8) {
+        if ((number & 0x8) == 0x8) {
             graphics_draw_line(ctx, tl, bl);
         }
     } else {
@@ -55,19 +58,40 @@ static void s_layer_update_proc(struct Layer *layer, GContext *ctx) {
     }
 }
 
+static void s_layer_update_proc(struct Layer *layer, GContext *ctx) {
+    XeviNumberLayer *xevi_layer = (XeviNumberLayer*)layer_get_data(layer);
+    GRect bounds = layer_get_bounds(layer);
+
+#if PBL_PLATFORM_BASALT
+    graphics_context_set_antialiased(ctx, true);
+    graphics_context_set_stroke_width(ctx, STROKE_WIDTH);
+#endif
+    // draw GEN_OLD
+    if (xevi_layer->number[GEN_NEW] != xevi_layer->number[GEN_OLD]) {
+        graphics_context_set_stroke_color(ctx, xevi_layer->color[GEN_OLD]);
+        s_draw_number(ctx, bounds, xevi_layer->number[GEN_OLD]);
+    }
+
+    // draw GEN_NEW
+    graphics_context_set_stroke_color(ctx, xevi_layer->color[GEN_NEW]);
+    s_draw_number(ctx, bounds, xevi_layer->number[GEN_NEW]);
+}
+
 XeviNumberLayer *xevi_number_layer_create(GRect frame) {
     XeviNumberLayer *xevi_layer = NULL;
 
     Layer *layer = layer_create_with_data(frame, sizeof(XeviNumberLayer));
     if (layer != NULL) {
+        // layer
+        layer_set_update_proc(layer, s_layer_update_proc);
+
         // set member
         xevi_layer = (XeviNumberLayer*)layer_get_data(layer);
         xevi_layer->layer = layer;
-        xevi_layer->number = 0;
-        xevi_layer->color = GColorBlack;
-
-        // layer
-        layer_set_update_proc(layer, s_layer_update_proc);
+        xevi_layer->number[GEN_NEW] = 0;
+        xevi_layer->number[GEN_OLD] = 0;
+        xevi_layer->color[GEN_NEW] = COLOR_GEN_NEW;
+        xevi_layer->color[GEN_OLD] = COLOR_GEN_OLD;
     }
     return xevi_layer;
 }
@@ -81,14 +105,13 @@ Layer *xevi_number_layer_get_layer(XeviNumberLayer *xevi_layer) {
 }
 
 uint8_t xevi_number_layer_get_number(XeviNumberLayer *xevi_layer, uint8_t number) {
-    return xevi_layer->number;
+    return xevi_layer->number[GEN_NEW];
 }
 
 void xevi_number_layer_set_number(XeviNumberLayer *xevi_layer, uint8_t number) {
     if (number <= XEVI_NUMBER_MAX) {
-        xevi_layer->number = number;
-    } else {
-        xevi_layer->number = XEVI_NUMBER_INVALID;
+        xevi_layer->number[GEN_OLD] = xevi_layer->number[GEN_NEW];
+        xevi_layer->number[GEN_NEW] = number;
+        layer_mark_dirty(xevi_number_layer_get_layer(xevi_layer));
     }
-    layer_mark_dirty(xevi_number_layer_get_layer(xevi_layer));
 }
